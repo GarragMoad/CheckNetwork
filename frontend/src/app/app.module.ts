57d4abe9772f77dc741f7334095c8626297753b6
Garrag Moad <garragmouad8@gmail.com>
2025-04-17 12:50:00 +0200

    dashboards angular ok

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { NodesListComponent } from './nodes-list/nodes-list.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { MatListModule } from '@angular/material/list';
     BrowserModule,
     AppRoutingModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/nodes-list/nodes-list.component.ts
+++ frontend/src/app/nodes-list/nodes-list.component.ts
@@ -1,22 +1,36 @@
-// nodes-list.component.ts
 import { Component, OnInit } from '@angular/core';
 import { WebSocketService } from '../services/web-socket.service';
-
+import { GetDashboardService } from '../services/get-dashboard.service';
 
 @Component({
   selector: 'app-nodes-list',
   templateUrl: './nodes-list.component.html',
-  styleUrls: ['./nodes-list.component.scss']
 })
 export class NodesListComponent implements OnInit {
   nodes: any[] = [];
 
-  constructor(private webSocketService: WebSocketService) {}
+  constructor(
+    private webSocketService: WebSocketService,
+    private getDashboardService: GetDashboardService
+  ) {}
 
   ngOnInit(): void {
     this.webSocketService.nodes$.subscribe(nodes => {
       this.nodes = nodes;
-      console.log('Nodes updated:', nodes);
+      this.enrichWithGrafanaUrls();
+    });
+  }
+
+  enrichWithGrafanaUrls(): void {
+    this.nodes.forEach(node => {
+      const title = `Interface Metrics - ${node.ip}`;
+      this.getDashboardService.getDashboardUrl(title).subscribe({
+        next: (url: any) => node.grafanaUrl = url,
+        error: (err: any) => {
+          console.warn(`No dashboard for ${node.ip}`, err);
+          node.grafanaUrl = null;
+        }
+      });
     });
   }
 
